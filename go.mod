5a51526064b6d53566b15471b35a627269711018
Christian <christian_mahardhika@telkomsel.co.id>
2024-04-26 19:58:07 +0700

    Update dependencies in go.mod and go.sum files

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module user-service
 go 1.22.0
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.19.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.9
 	github.com/redis/go-redis/v9 v9.5.1
 	github.com/stretchr/testify v1.8.4
+	github.com/valyala/fasthttp v1.51.0
 	golang.org/x/crypto v0.19.0
 )
 
@@ -41,7 +43,6 @@ require (
 	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.elastic.co/fastjson v1.1.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
@@ -51,6 +52,7 @@ require (
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 )
---
+++ internal/module/user/handler/handler_test.go
@@ -0,0 +1,326 @@
+package handler_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+	"user-service/internal/module/user/handler"
+	"user-service/internal/module/user/mocks"
+	"user-service/internal/module/user/models/request"
+	"user-service/internal/module/user/models/response"
+	"user-service/internal/pkg/log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	usecases      *mocks.Usecases
+	logTest       *log.Logger
+	h             handler.UserHandler
+	validatorTest *validator.Validate
+	app           *fiber.App
+)
+
+func setup() {
+	usecases = new(mocks.Usecases)
+	logZap := log.SetupLogger()
+	log.Init(logZap)
+	logTest := log.GetLogger()
+	validatorTest = validator.New()
+	h = handler.UserHandler{
+		Log:       logTest,
+		Usecase:   usecases,
+		Validator: validatorTest,
+	}
+
+	app = fiber.New()
+
+}
+
+func teardown() {
+	usecases = nil
+	logTest = nil
+	h = handler.UserHandler{}
+}
+
+func TestRegister(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.Register{
+			// Set the required fields of the request struct
+			Email:    "test@test.com",
+			Password: "password",
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("POST", "/register", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("POST")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/register")
+
+		// Set the expected response
+		usecases.On("Register", ctx.Context(), &payload).Return(nil)
+
+		// Call the function
+		err := h.Register(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+
+	})
+}
+
+func TestLogin(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.Login{
+			// Set the required fields of the request struct
+			Email:    "test@test.com",
+			Password: "password",
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("POST", "/login", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("POST")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/login")
+
+		// Set the expected response
+		usecases.On("Login", ctx.Context(), &payload).Return(response.LoginResponse{
+			Token:     "token",
+			ExpiredAt: 0,
+		}, nil)
+
+		// Call the function
+		err := h.Login(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestGetUser(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.GetUser{
+			// Set the required fields of the request struct
+			ID: 1,
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("GET", "/user", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("GET")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/user")
+		ctx.Locals("userID", 1)
+
+		// Set the expected response
+		usecases.On("GetUser", ctx.Context(), &payload).Return(response.GetUserResponse{
+			ID:    1,
+			Email: "test@test.com",
+		}, nil)
+
+		// Call the function
+		err := h.GetUser(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestUpdateUser(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.UpdateUser{
+			// Set the required fields of the request struct
+			ID: 1,
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("PUT", "/user", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("PUT")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/user")
+
+		// Set the expected response
+		usecases.On("UpdateUser", ctx.Context(), &payload).Return(nil)
+
+		// Call the function
+		err := h.UpdateUser(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestCreateProfile(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.CreateProfile{
+			// Set the required fields of the request struct
+			UserID: 1,
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("POST", "/profile", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("POST")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/profile")
+
+		// Set the expected response
+		usecases.On("CreateProfile", ctx.Context(), &payload).Return(nil)
+
+		// Call the function
+		err := h.CreateProfile(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestGetProfile(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.GetProfile{
+			// Set the required fields of the request struct
+			UserID: 1,
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("GET", "/profile", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("GET")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/profile")
+		ctx.Locals("userID", 1)
+
+		// Set the expected response
+		usecases.On("GetProfile", ctx.Context(), &payload).Return(response.GetProfileResponse{
+			UserID: 1,
+		}, nil)
+
+		// Call the function
+		err := h.GetProfile(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestUpdateProfile(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare test data
+		payload := request.UpdateProfile{
+			// Set the required fields of the request struct
+			UserID: 1,
+		}
+
+		// Prepare the request
+		reqBody, _ := json.Marshal(payload)
+		req := httptest.NewRequest("PUT", "/profile", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().SetBody(reqBody)
+		ctx.Request().Header.SetMethod("PUT")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/profile")
+
+		// Set the expected response
+		usecases.On("UpdateProfile", ctx.Context(), &payload).Return(nil)
+
+		// Call the function
+		err := h.UpdateProfile(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
+
+func TestValidateToken(t *testing.T) {
+	setup()
+	defer teardown()
+
+	t.Run("success", func(t *testing.T) {
+		// Prepare the request
+		req := httptest.NewRequest("GET", "/private/user/validate", nil)
+		req.Header.Set("Content-Type", "application/json")
+
+		ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+		// Set the request body
+		ctx.Request().Header.SetMethod("GET")
+		ctx.Request().Header.SetContentType("application/json")
+		ctx.Request().SetRequestURI("/private/user/validate")
+
+		// Set the expected response
+		usecases.On("ValidateToken", ctx.Context()).Return(nil)
+
+		// Call the function
+		err := h.ValidateToken(ctx)
+
+		// assert the result
+		assert.Nil(t, err)
+	})
+}
